208d50f8df4601dc19e5dd109b4f7cfd6b7b6989
Anton <profa142@gmail.com>
2023-09-23 03:12:53 +0300

    Refactor safe_call

--- arrayfire_wrapper/lib/_utility.py
+++ arrayfire_wrapper/lib/_utility.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 import ctypes
 from typing import Any
 
-from arrayfire_wrapper._backend import get_backend
-from arrayfire_wrapper.defines import AFArray
+from arrayfire_wrapper._backend import Backend, get_backend
+from arrayfire_wrapper.defines import AFArray, CDimT
+from arrayfire_wrapper.dtypes import to_str
 from arrayfire_wrapper.lib._broadcast import bcast_var
-from arrayfire_wrapper.lib._error_handler import safe_call
+from arrayfire_wrapper.lib._constants import ErrorCodes
 
 
 def binary_op(c_func_name: str, lhs: AFArray, rhs: AFArray, /) -> AFArray:
@@ -23,4 +24,16 @@ def unary_op(c_func_name: str, arr: AFArray, /) -> AFArray:
 
 def call_from_clib(func_name: str, *args: Any, clib_prefix: str = "af", **kwargs: Any) -> None:
     backend = get_backend()
-    safe_call(getattr(backend.clib, f"{clib_prefix}_{func_name}")(*args, **kwargs))
+    c_err = getattr(backend.clib, f"{clib_prefix}_{func_name}")(*args, **kwargs)
+
+    if c_err == ErrorCodes.NONE.value:
+        return
+
+    _process_error(backend)
+
+
+def _process_error(backend: Backend) -> None:
+    err_str = ctypes.c_char_p(0)
+    err_len = CDimT(0)
+    backend.clib.af_get_last_error(ctypes.pointer(err_str), ctypes.pointer(err_len))
+    raise RuntimeError(to_str(err_str))
