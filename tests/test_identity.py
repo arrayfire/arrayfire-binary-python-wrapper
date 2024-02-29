bb0209be1b949a7c2fabe1f616aa6361fd025d99
Chaluvadi <saketh.chaluvadi@intel.com>
2024-02-29 13:36:21 -0500

    Fixed import formatting, black and flake8 automatic checks

--- tests/test_identity.py
+++ tests/test_identity.py
@@ -3,9 +3,7 @@ import random
 import pytest
 
 import arrayfire_wrapper.dtypes as dtypes
-from arrayfire_wrapper.lib.create_and_modify_array.create_array.identity import identity
-from arrayfire_wrapper.lib.create_and_modify_array.manage_array import get_dims, get_type
-from arrayfire_wrapper.lib.create_and_modify_array.manage_device import get_dbl_support
+import arrayfire_wrapper.lib as wrapper
 
 
 @pytest.mark.parametrize(
@@ -22,9 +20,9 @@ def test_identity_shape(shape: tuple) -> None:
     """Test if identity creates an array with the correct shape"""
     dtype = dtypes.s16
 
-    result = identity(shape, dtype)
+    result = wrapper.identity(shape, dtype)
 
-    assert get_dims(result)[0:len(shape)] == shape
+    assert wrapper.get_dims(result)[0 : len(shape)] == shape  # noqa: E203
 
 
 def test_identity_invalid_shape() -> None:
@@ -39,7 +37,7 @@ def test_identity_invalid_shape() -> None:
         )
         dtype = dtypes.s16
 
-        identity(invalid_shape, dtype)
+        wrapper.identity(invalid_shape, dtype)
 
     assert f"CShape.__init__() takes from 1 to 5 positional arguments but {len(invalid_shape) + 1} were given" in str(
         excinfo.value
@@ -50,9 +48,9 @@ def test_identity_nonsquare_shape() -> None:
     dtype = dtypes.s16
     shape = (5, 6)
 
-    result = identity(shape, dtype)
+    result = wrapper.identity(shape, dtype)
 
-    assert get_dims(result)[0:len(shape)] == shape
+    assert wrapper.get_dims(result)[0 : len(shape)] == shape  # noqa: E203
 
 
 @pytest.mark.parametrize(
@@ -61,12 +59,12 @@ def test_identity_nonsquare_shape() -> None:
 )
 def test_identity_dtype(dtype_index: int) -> None:
     """Test if identity creates an array with the correct dtype"""
-    if dtype_index in [2, 3] and not get_dbl_support():
+    if dtype_index in [2, 3] and not wrapper.get_dbl_support():
         pytest.skip()
 
     shape = (5, 5)
     dtype = dtypes.c_api_value_to_dtype(dtype_index)
 
-    result = identity(shape, dtype)
+    result = wrapper.identity(shape, dtype)
 
-    assert dtypes.c_api_value_to_dtype(get_type(result)) == dtype
+    assert dtypes.c_api_value_to_dtype(wrapper.get_type(result)) == dtype
