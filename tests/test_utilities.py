5ce3c14ac7fca93e9003dde7c3f1f89b0e9a80b4
Chaluvadi <saketh.chaluvadi@intel.com>
2024-03-13 10:26:04 -0400

    Fixed utility functions

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,19 +1,13 @@
-import arrayfire_wrapper.lib as wrapper
 import pytest
 
-from arrayfire_wrapper.dtypes import f64, f16, c32, c64, Dtype
-
-
-def is_cmplx_type(dtype: Dtype) -> bool:
-    """Checks to see if the specified type is a complex type"""
-    return dtype == c32 or dtype == c64
+import arrayfire_wrapper.lib as wrapper
+from arrayfire_wrapper.dtypes import Dtype, c64, f16, f64
 
 
 def check_type_supported(dtype: Dtype) -> None:
     """Checks to see if the specified type is supported by the current system"""
     if dtype in [f64, c64] and not wrapper.get_dbl_support():
-        pytest.skip("Device does not support doubel types.")
+        pytest.skip("Device does not support double types")
 
     if dtype == f16 and not wrapper.get_half_support():
         pytest.skip("Device does not support half types.")
-
