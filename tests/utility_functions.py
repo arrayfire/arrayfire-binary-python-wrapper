c414f5fb6f4b361978c3a04821a375ff3f44c328
Azeez <95100110+AzeezIsh@users.noreply.github.com>
2024-03-22 18:59:18 -0400

    Trigonometry Testing (#35)
    * Added float types method and boolean type.
    
    * Incorperated Utility Functions, added checkstyle
    
    Checked for all dtype compatibility issues where needed.
    
    * Ensuring checkstyle was applied
    
    * Adhered to black checkstyle.
    
    ---------
    
    Co-authored-by: AzeezIsh <azishaqui@gmail.com>

--- tests/utility_functions.py
+++ tests/utility_functions.py
@@ -1,14 +1,13 @@
 import pytest
 
 import arrayfire_wrapper.lib as wrapper
-from arrayfire_wrapper.dtypes import Dtype, c32, c64, f16, f32, f64, s16, s32, s64, u8, u16, u32, u64
+from arrayfire_wrapper.dtypes import Dtype, b8, c32, c64, f16, f32, f64, s16, s32, s64, u8, u16, u32, u64
 
 
 def check_type_supported(dtype: Dtype) -> None:
     """Checks to see if the specified type is supported by the current system"""
     if dtype in [f64, c64] and not wrapper.get_dbl_support():
         pytest.skip("Device does not support double types")
-
     if dtype == f16 and not wrapper.get_half_support():
         pytest.skip("Device does not support half types.")
 
@@ -25,4 +24,9 @@ def get_real_types() -> list:
 
 def get_all_types() -> list:
     """Returns all types"""
-    return [s16, s32, s64, u8, u16, u32, u64, f16, f32, f64, c32, c64]
+    return [b8, s16, s32, s64, u8, u16, u32, u64, f16, f32, f64, c32, c64]
+
+
+def get_float_types() -> list:
+    """Returns all types"""
+    return [f16, f32, f64]
