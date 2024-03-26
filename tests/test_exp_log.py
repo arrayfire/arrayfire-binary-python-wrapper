034af6ca1726210aef6fd121d7c708bbc0ae9ed8
AzeezIsh <azishaqui@gmail.com>
2024-03-26 15:31:54 -0400

    Added all dtype coverage, removed dmap.

--- tests/test_exp_log.py
+++ tests/test_exp_log.py
@@ -4,35 +4,7 @@ import pytest
 
 import arrayfire_wrapper.dtypes as dtype
 import arrayfire_wrapper.lib as wrapper
-
-dtype_map = {
-    "int16": dtype.s16,
-    "int32": dtype.s32,
-    "int64": dtype.s64,
-    "uint8": dtype.u8,
-    "uint16": dtype.u16,
-    "uint32": dtype.u32,
-    "uint64": dtype.u64,
-    "float16": dtype.f16,
-    "float32": dtype.f32,
-    # 'float64': dtype.f64,
-    # 'complex64': dtype.c64,
-    # 'complex32': dtype.c32,
-    "bool": dtype.b8,
-    "s16": dtype.s16,
-    "s32": dtype.s32,
-    "s64": dtype.s64,
-    "u8": dtype.u8,
-    "u16": dtype.u16,
-    "u32": dtype.u32,
-    "u64": dtype.u64,
-    "f16": dtype.f16,
-    "f32": dtype.f32,
-    # 'f64': dtype.f64,
-    # 'c32': dtype.c32,
-    # 'c64': dtype.c64,
-    "b8": dtype.b8,
-}
+from tests.utility_functions import check_type_supported, get_all_types, get_float_types
 
 
 @pytest.mark.parametrize(
@@ -40,15 +12,15 @@ dtype_map = {
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_cbrt_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test cube root operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.cbrt(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -72,15 +44,15 @@ def test_cbrt_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_erf_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test gaussian error operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.erf(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -104,15 +76,15 @@ def test_erf_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_erfc_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test gaussian error complement operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.erfc(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -136,15 +108,15 @@ def test_erfc_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_exp_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test exponent operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.exp(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -168,15 +140,15 @@ def test_exp_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_exp1_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test exponent - 1 operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.expm1(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -200,15 +172,15 @@ def test_expm1_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_fac_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test exponent operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.factorial(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -238,9 +210,10 @@ def test_fac_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_lgamma_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test lgamma operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.lgamma(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -264,15 +237,15 @@ def test_lgamma_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_log_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test log operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.log(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -296,15 +269,15 @@ def test_log_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_log10_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test log10 operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.log10(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -328,15 +301,15 @@ def test_log10_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_log1p_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test natural logarithm of 1 + input operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.log1p(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -360,15 +333,15 @@ def test_log1p_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_log2_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test log2 operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.log2(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -392,15 +365,15 @@ def test_log2_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_pow_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test power operation across all supported data types."""
+    check_type_supported(dtype_name)
     lhs = wrapper.randu(shape, dtype_name)
     rhs = wrapper.randu(shape, dtype_name)
     result = wrapper.pow(lhs, rhs)
@@ -425,15 +398,15 @@ def test_pow_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_root_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test root operation across all supported data types."""
+    check_type_supported(dtype_name)
     lhs = wrapper.randu(shape, dtype_name)
     rhs = wrapper.randu(shape, dtype_name)
     result = wrapper.root(lhs, rhs)
@@ -464,9 +437,10 @@ def test_root_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_pow2_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test 2 to power operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.pow2(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -490,15 +464,15 @@ def test_pow2_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_rsqrt_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test reciprocal square root operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.rsqrt(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -522,15 +496,15 @@ def test_rsqrt_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_sqrt_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test  square root operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.sqrt(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -554,15 +528,15 @@ def test_sqrt_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_tgamma_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test gamma operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.tgamma(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
@@ -586,15 +560,15 @@ def test_tgamma_unsupported_dtypes(invdtypes: dtype.Dtype) -> None:
     [
         (),
         (random.randint(1, 10),),
-        (random.randint(1, 10),),
         (random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
     ],
 )
-@pytest.mark.parametrize("dtype_name", dtype_map.values())
+@pytest.mark.parametrize("dtype_name", get_all_types())
 def test_sigmoid_shape_dtypes(shape: tuple, dtype_name: dtype.Dtype) -> None:
     """Test sigmoid operation across all supported data types."""
+    check_type_supported(dtype_name)
     values = wrapper.randu(shape, dtype_name)
     result = wrapper.sigmoid(values)
     assert wrapper.get_dims(result)[0 : len(shape)] == shape, f"failed for shape: {shape}"  # noqa
