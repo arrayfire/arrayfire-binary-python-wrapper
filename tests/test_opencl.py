4a505dc2967d7866d9fa58cab5a3354246952ae7
sakchal <chaluvadi.saketh@gmail.com>
2024-05-02 11:51:53 -0400

    fixed test_opencl mypy issues

--- tests/test_opencl.py
+++ tests/test_opencl.py
@@ -1,26 +1,26 @@
 import arrayfire_wrapper.lib.interface_functions.opencl as cl
 
 
-def test_get_context_type():
+def test_get_context_type() -> None:
     assert isinstance(cl.get_context(), int)
 
 
-def test_get_queue_type():
+def test_get_queue_type() -> None:
     assert isinstance(cl.get_queue(), int)
 
 
-def test_get_device_id():
+def test_get_device_id() -> None:
     assert isinstance(cl.get_device_id(), int)
 
 
-def test_set_device_id():
+def test_set_device_id() -> None:
     cl.set_device_id(0)
     assert cl.get_device_id() == 0
 
 
-def test_get_device_type():
+def test_get_device_type() -> None:
     assert cl.get_device_type() == cl.DeviceType.GPU  # change according to device
 
 
-def test_get_platform():
+def test_get_platform() -> None:
     assert cl.get_platform() == cl.PlatformType.INTEL  # change according to platform
